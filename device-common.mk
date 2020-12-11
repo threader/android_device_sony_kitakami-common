1526ab6507f0733b53c20914ac51f9085a38d5d1
Bernhard Thoben <berni-the-bear@web.de>
2020-12-11 18:29:35 +0100

    kitakami-common: Bring back FM-Radio!
    Change-Id: Ib5e6848034516afa6dad343afbb2bff5bab4281f
    (cherry picked from commit a45fe9ab9a38a1e254dacbeda6e35a024aa337e1)

--- device-common.mk
+++ device-common.mk
@@ -151,6 +151,13 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8994
 
+
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    brcm-uim-sysfs \
+    libfmradio.v4l2-fm
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
